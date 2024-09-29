61f097a8686aeae50d3c4e5ec00187e9409b9035
flutter <alkadafifirnawan1@gmail.com>
2024-09-29 18:39:32 +0700

    project hello_world

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'basic_widgets/text_widget.dart';
+import 'basic_widgets/image_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Alkadafi Firnawan'),
     );
   }
 }
@@ -105,7 +107,8 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
+            const MyTextWidget(),
+            Text(
               'You have pushed the button this many times:',
             ),
             Text(
